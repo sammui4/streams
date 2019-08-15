9f85f51f3bcea1e8801db6e7632d9a2e601b9ded
sammui4 <490454041@qq.com>
2019-08-15 15:34:20 +0800

    改成移动

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
  * @Author: w
  * @Date: 2019-08-15 09:26:08
  * @LastEditors: w
- * @LastEditTime: 2019-08-15 14:30:36
+ * @LastEditTime: 2019-08-15 15:34:07
  */
 var fs = require('fs');
 var request = require('request');
@@ -14,9 +14,19 @@ function downloadFile(uri,filename,callback){
   requests.on('error',(err)=>{
   })
   requests.on('response', function(response) {
-    requests.pipe(fs.createWriteStream(filename)).on('close',callback)
+    let dir = fs.mkdtempSync(path.join(__dirname,'upgrade_'));
+    let paths = path.join(dir,filename);
+    requests.pipe(fs.createWriteStream(paths)).on('close',()=>{
+      fs.rename(paths, path.join(__dirname,filename),function(){
+        fs.rmdir(dir,(err )=>{
+          if(err){
+            return
+          }
+          callback()
+        });
+      });
+    })
   })
-  
 }
 
 var fileUrl  = 'http://localhost:888/client/win-unpacked/resources/app.asar';
